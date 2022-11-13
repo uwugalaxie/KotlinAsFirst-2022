f35c7ee6095e6f62e225df7489505dfdf85e2757
gal4x <gal4xie@gmail.com>
2022-11-13 16:40:33 +0300

    .

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -357,7 +357,7 @@ fun bagPacking(treasures: Map<String, Pair<Int, Int>>, capacity: Int): Set<Strin
     var load = capacity
 
     for ((m, p) in treasures) {
-        value.add(Pair(m, p.second))
+        value.add(Pair(m, p.second / p.first))
     }
     value.sortByDescending { it.second }
 
