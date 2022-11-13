7194599e64ecd25ea3f6e5be7f0e472a99aa5977
gal4x <gal4xie@gmail.com>
2022-11-13 19:09:59 +0300

    .

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -357,7 +357,7 @@ fun bagPacking(treasures: Map<String, Pair<Int, Int>>, capacity: Int): Set<Strin
     var load = capacity
 
     for ((m, p) in treasures) {
-        value.add(Pair(m, p.first / p.second))
+        value.add(Pair(m, capacity / p.second))
     }
     value.sortByDescending { it.second }
 
--- test/lesson5/task1/Tests.kt
+++ test/lesson5/task1/Tests.kt
@@ -330,6 +330,13 @@ class Tests {
     @Tag("8")
     fun bagPacking() {
         assertEquals(
+            setOf("0"),
+            bagPacking(
+                mapOf("0" to (1 to 2), "1" to (2 to 1)),
+                1
+            )
+        )
+        assertEquals(
             setOf("Кубок"),
             bagPacking(
                 mapOf("Кубок" to (500 to 2000), "Слиток" to (1000 to 5000)),
